4127bd0b8aa31ba430b4f1eae37fe100c9a086a9
mysticJawel <98066287+mysticJawel@users.noreply.github.com>
2022-06-09 17:13:24 +0200

    popup show and hide

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,28 @@
 const button = document.querySelector('button');
+const popupWrapper = document.querySelector('.popupWrapper')
+const popup = document.querySelector('.popup');
+
+let counter = 0;
+let popupActive =false;
+
 button.addEventListener('click',()=>{
-    alert("Alert box!");
-})
+    if(!popupActive){
+    popupWrapper.style.display = 'block'
+    counter++;
+    console.log('counter: '+counter)
+    console.log('popup: '+popupActive)
+    popupActive=true;}})
 
 
+    popupWrapper.addEventListener('click', function(e){
+    console.log('popup: '+popupActive)
+    if(popupActive){
+	if (popup.contains(e.target)){
+  	console.log("Clicked in Box");
 
-
-
+  } else{
+  	console.log("Clicked outside Box");
+    console.log('counter: '+counter);
+    popupWrapper.style.display = 'none'
+    popupActive = false;
+  }}})
