17f2e52d79ad9cd65ca06344567991d58da33f3d
mysticJawel <98066287+mysticJawel@users.noreply.github.com>
2022-06-09 21:39:54 +0200

    popup complete

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,23 @@
 const button = document.querySelector('button');
 const popupWrapper = document.querySelector('.popupWrapper')
 const popup = document.querySelector('.popup');
+const popupBoldText = document.querySelector('.popupBoldText');
+const buttonX = document.querySelector('.buttonX');
+const buttonReset = document.querySelector('.buttonReset');
 
 let counter = 0;
 let popupActive =false;
 
 button.addEventListener('click',()=>{
+  if(counter>=4){
+    buttonReset.style.display = 'block';
+  }
     if(!popupActive){
     popupWrapper.style.display = 'block'
     counter++;
     console.log('counter: '+counter)
     console.log('popup: '+popupActive)
+    popupBoldText.textContent = counter+ ' times';
     popupActive=true;}})
 
 
@@ -18,11 +25,21 @@ button.addEventListener('click',()=>{
     console.log('popup: '+popupActive)
     if(popupActive){
 	if (popup.contains(e.target)){
-  	console.log("Clicked in Box");
-
+  	console.log("in Popup");
   } else{
-  	console.log("Clicked outside Box");
+  	console.log("outside Popup");
     console.log('counter: '+counter);
     popupWrapper.style.display = 'none'
     popupActive = false;
   }}})
+
+  buttonX.addEventListener('click', function(e){
+    popupWrapper.style.display = 'none'
+    popupActive = false;
+  })
+
+  buttonReset.addEventListener('click',()=>{
+    counter=0;
+    popupBoldText.textContent = counter+ ' times';
+    buttonReset.style.display = 'none';
+  });
