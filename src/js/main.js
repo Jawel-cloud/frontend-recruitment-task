c9b457b78722a4f635174365a0f9b9ed69eae6ce
mysticJawel <98066287+mysticJawel@users.noreply.github.com>
2022-06-16 17:28:46 +0200

    loader

--- src/js/main.js
+++ src/js/main.js
@@ -9,6 +9,33 @@ let counterInStorage = localStorage.getItem("counterInStorage") !== null ? JSON.
 let counter = parseInt(counterInStorage);
 let popupActive = false;
 
+const table = document.querySelector('.table')
+const tbody = document.querySelector('tbody');
+const loader = document.querySelector('.loader')
+let timer;
+
+const showTable = () => {
+  loader.style.display = "none";
+  table.style.display = "block";
+}
+
+const loading = () => {
+  setTimeout(showTable, 1500);
+}
+const fetchFunction = () => {
+  fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(data => Array.from(data))
+    .then(data => {
+      console.log(data.length)
+      for (let i = 0; i < data.length; i++) {
+        const tr = document.createElement('tr')
+        tr.innerHTML = `<td>${data[i].name} </td><td>${data[i].email} </td><td class="nomobile">${data[i].address.city}, ${data[i].address.street}, ${data[i].address.suite}</td><td class="nomobile">${data[i].phone}</td><td class="nomobile">${data[i].company.name}</td>`
+        tbody.appendChild(tr)
+      }
+    })
+}
+
 button.addEventListener('click', () => {
   if (counter >= 4) {
     buttonReset.style.display = 'block';
@@ -21,6 +48,9 @@ button.addEventListener('click', () => {
     popupBoldText.textContent = counter + ' times';
     popupActive = true;
   }
+  tbody.innerHTML = '';
+  loading();
+  fetchFunction();
 })
 
 
@@ -28,6 +58,8 @@ popupWrapper.addEventListener('click', function (e) {
   if (popupActive) {
     if (popup.contains(e.target)) {} else {
       popupWrapper.style.display = 'none'
+      loader.style.display = "block";
+      table.style.display = "none";
       popupActive = false;
     }
   }
@@ -44,21 +76,3 @@ buttonReset.addEventListener('click', () => {
   popupBoldText.textContent = counter + ' times';
   buttonReset.style.display = 'none';
 });
-
-
-
-const tbody = document.querySelector('tbody');
-const fetchFunction = () => {
-  fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then(data => Array.from(data))
-    .then(data => {
-      console.log(data.length)
-      for (let i = 0; i < data.length; i++) {
-        const tr = document.createElement('tr')
-        tr.innerHTML = `<td>${data[i].name} </td><td>${data[i].email} </td><td class="nomobile">${data[i].address.city}, ${data[i].address.street}, ${data[i].address.suite}</td><td class="nomobile">${data[i].phone}</td><td class="nomobile">${data[i].company.name}</td>`
-        tbody.appendChild(tr)
-      }
-    })
-}
-fetchFunction();
